e5f0abd3c71808bd9cff7ca419aaf5be6eeb5d36
Josue Miguel <josmiguel92@gmail.com>
2021-02-09 23:39:15 -0500

    fix style

--- Theme/header.php
+++ Theme/header.php
@@ -3,7 +3,7 @@
 <head>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
 <link href='' rel='stylesheet' type='text/css'>
-<meta charset="<?php bloginfo( 'charset' );?>">
+<meta charset="<?php bloginfo('charset');?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="description" content="Senses of Cuba is your European DMC with a main office in the heart of Havana, close to all important sights and hotels. Focussing on a sophisticated upmarket clientele with tailormade programs for FITs, luxury travellers, special interest groups, incentive groups and more.">
 <meta name="keywords" content="Tailormade FITs & Groups, Special Interest Tours, Luxury Travel, Incentives & Events, Meeting & Conferences, Vips & Delegations, Accomodation, Excursion, Rental Cars, Cuba Excursions, Cuba Experiencies.">
@@ -20,7 +20,7 @@
 
 <?php echo getGoogleTagManagerBody(); ?>
 
-<?php    
+<?php
   $newsQuery=new WP_Query(array('category_name'  => 'News', 'lang' => 'en'));
   $eventsQuery=new WP_Query(array('category_name'  => 'Events', 'lang' => 'en'));
   $homeUrl = home_url();
@@ -49,15 +49,13 @@
     <div aria-haspopup="true" class="dropdown text-primary" style=" color: #fdc416 !important;">
         <a data-toggle="dropdown" class="dropdown-toggle nav-link">
             <i class="flag-icon flag-icon-en"></i>
-            <span class="no-icon">   <?php
-
-//                echo (new PLL_Language(pll_current_language()))->flag;
-$currentLang = pll_current_language('OBJECT');
-echo  $currentLang->name. " " .$currentLang->flag. " ";
-
-//                var_dump();
+            <span class="no-icon">
+                <?php
+                    $currentLang = pll_current_language('OBJECT');
+                    echo $currentLang->name. " " .$currentLang->flag. " ";
                 ?>
-            </span></a>
+            </span>
+        </a>
         <!---->
         <ul class="dropdown-menu" style="background: rgba(0,0,0,.9);">
             <?php pll_the_languages(['show_flags' => true, 'show_names'=> true ]); ?>
@@ -69,22 +67,21 @@ echo  $currentLang->name. " " .$currentLang->flag. " ";
   <a href="<?php echo $homeUrl; ?>#ourTeam" onclick="activeMenuFunction(this);"><?php echoTranslatedString('navOurTeam'); ?></a>
   <a href="<?php echo $homeUrl; ?>#ProductSection" onclick="activeMenuFunction(this);"><?php echoTranslatedString('navProductsLeisure'); ?></a>
 
-    <?php
-    if($fileURL = getIncentivesFileURL())
-    {
-        ?>
+    <?php if ($fileURL = getIncentivesFileURL()): ?>
         <a href="<?php echo $fileURL; ?>" target="_blank" onclick="activeMenuFunction(this);"><?php echoTranslatedString('navProductsMICE'); ?></a>
-    <?php
-    }
-    ?>
+    <?php endif;?>
+
   <a href="<?php echo $homeUrl; ?>#MeetUs" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navMeetUs'); ?></a>
-  <a href="#Jobs" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navJobs'); ?></a>
-  <?php if($eventsQuery->have_posts()):?>  
-  <a href="#Events" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navEvents'); ?></a>
-  <?php endif;?>
-  <?php if($newsQuery->have_posts()):?>  
-  <a href="<?php echo $homeUrl; ?>#News" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navNews'); ?></a>
-  <?php endif;?>
+  <a href="<?php echo $homeUrl; ?>#Jobs" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navJobs'); ?></a>
+
+    <?php if ($eventsQuery->have_posts()):?>
+        <a href="<?php echo $homeUrl; ?>#Events" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navEvents'); ?></a>
+    <?php endif;?>
+
+    <?php if ($newsQuery->have_posts()):?>
+        <a href="<?php echo $homeUrl; ?>#News" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navNews'); ?></a>
+    <?php endif;?>
+
   <a href="<?php echo $homeUrl; ?>#Testimonial" onclick="activeMenuFunction(this)"><?php echoTranslatedString('navTestimonial'); ?></a>
   
   <a href="https://infonet.sensesofcuba.com/" target="_blank" onclick="activeMenuFunction(this)">Infonet</a>
@@ -118,7 +115,8 @@ echo  $currentLang->name. " " .$currentLang->flag. " ";
    <div class="collapse navbar-collapse" id="navbarSupportedContent" >
 	<a>Logo</a>
     <?php
-        wp_nav_menu( array(
+        wp_nav_menu(
+                    array(
                 'menu'              => 'header_Menu',
                 'theme_location'    => 'header_Menu',
                 'depth'             => 2,
@@ -128,7 +126,7 @@ echo  $currentLang->name. " " .$currentLang->flag. " ";
                 'menu_class'        => 'nav navbar-nav',
                 'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                 'walker'            => new WP_Bootstrap_Navwalker())
-            );
+                );
         ?>
     </div>
     </nav>-->
