576c42fcf2527bb36069063160c179e4dd1f974a
Josue Miguel <josmiguel92@gmail.com>
2022-01-14 21:03:11 -0500

    remove attachement calls on theme

--- Theme/page.php
+++ Theme/page.php
@@ -25,26 +25,6 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );?>
                             <div class="entry-content">
                                 <?php the_content();?>
 
-                                 <?php
-                                  //retrieve all Attachments for the 'attachments' instance of post 123
-                                $attachments = new Attachments( 'attachments' );?>
-                                <?php if( $attachments->exist()) : ?>
-                                <h3>Attachments</h3>
-
-                                  <div class="row">
-                                    <?php while( $attachment = $attachments->get() ) : ?>
-                                        <div class="col-sm-4">
-                                        <figure class="figure ">
-                                        <img class=" figure-img mw-100 img-thumbnail w-100 h-25"  src="<?php echo $attachments->url() ?>" >
-                                            <figcaption class="figure-caption"><?php echo $attachments->field( 'caption' ) ?></figcaption>
-                                    </figure>
-                                            <div class="col-md-12">
-                                                <a href="<?php echo $attachments->url() ?>" class="btn btn-primary">Click to see the file.</a>
-                                            </div>
-                                    </div>
-                                    <?php endwhile; ?>
-                                  </div>
-                                <?php endif; ?>
                             </div><!-- entry-content -->
                         </article>
                     </div>
